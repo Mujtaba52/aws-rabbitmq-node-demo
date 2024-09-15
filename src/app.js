15b84ed0900c684434e58410596b870bba55570e
Mujtaba Hassan <mujhassan786@gmail.com>
2024-09-15 05:45:03 +0300

    update app.js file to call the receiver function

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import "dotenv/config";
-import taskRouter from "./routes/task.route"
+import taskRouter from "./routes/task.route.js";
+import { consumeFromQueue } from './mq/receiver.js';
 
 const app = express();
 app.use(express.json()); 
 app.use('/api',taskRouter)
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
+    consumeFromQueue('task_queue', (receivedData) => {
+        console.log('Received message:', receivedData);
+
+        // You can add any additional processing logic for received messages here
+    });
 });
